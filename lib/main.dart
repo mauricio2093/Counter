44a8b1b9329e526dee817018f9c52755c6a059c6
mauricio2093 <mauriciocadena2093@gmail.com>
2023-07-06 23:21:15 -0500

    01-fluttert

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,8 @@
+import 'package:aplication_01/presentation/screens/counter/counter_screen.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
@@ -11,9 +12,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        body: Center(child: Text('Hola Mundo !')),
-      ),
+      home: CounterScreen(),
     );
   }
 }
